e3c4a141e0005243364c220895e2f236d78ac78d
Taylor Ring <ringt@uni.coventry.ac.uk>
2019-10-31 10:18:21 +0000

    weekly backup

---
+++ Whip and Whisk Game/Assets/Person/MaterialSwapping.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwapping : MonoBehaviour {
+
+    public List<Material> ListOfMaterials;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GoThrough());
+	}
+
+    IEnumerator GoThrough()
+    {
+        int pointer = 0;
+        while (true)
+        {
+            GetComponent<SkinnedMeshRenderer>().material = ListOfMaterials[pointer];
+            pointer++;
+            if(pointer == ListOfMaterials.Count)
+            {
+                pointer = 0;
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
